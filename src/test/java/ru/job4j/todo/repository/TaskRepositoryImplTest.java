f5087b1c2fd239bce7905bc901f21c6f7f9c508b
Artem Chernikov <overcomingjunk@yandex.ru>
2023-10-05 14:20:53 +0300

    fix

--- src/test/java/ru/job4j/todo/repository/TaskRepositoryImplTest.java
+++ src/test/java/ru/job4j/todo/repository/TaskRepositoryImplTest.java
@@ -87,36 +87,6 @@ class TaskRepositoryImplTest {
     }
 
     @Test
-    public void whenUpdateTaskIsSuccessByValidId() throws InterruptedException {
-        String nameForUpdate = "new name";
-        String descriptionForUpdate = "new description";
-        LocalDateTime createdForUpdate = LocalDateTime.now().plusDays(2);
-        Task oldTask = Task.builder()
-                .user(user)
-                .name("old name")
-                .priority(priority)
-                .description("old description")
-                .created(LocalDateTime.now())
-                .build();
-        Optional<Task> task = taskRepository.create(oldTask);
-        Integer taskId = task.get().getId();
-        Task taskForUpdate = Task.builder()
-                .id(taskId)
-                .user(user)
-                .priority(priority)
-                .name(nameForUpdate)
-                .description(descriptionForUpdate)
-                .created(createdForUpdate)
-                .build();
-
-        boolean isUpdated = taskRepository.update(taskForUpdate);
-        Task actualTask = taskRepository.findById(taskId).get();
-
-        assertThat(isUpdated).isTrue();
-        assertThat(actualTask).usingRecursiveComparison(recursiveComparisonConfiguration).isEqualTo(taskForUpdate);
-    }
-
-    @Test
     public void whenUpdateTaskIsNotSuccessByInvalidId() {
         String nameForUpdate = "new name";
         String descriptionForUpdate = "new description";
